99c4a3d338c365466c9889489bb8666b5749b6d8
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-10-01 17:09:14 +0100

    Create FirstVariable.java

---
+++ FirstVariable.java
@@ -0,0 +1,16 @@
+class FirstVariable {
+  public static void main(String[] args) {
+    // Declare and initialise a variable
+    String message = "Initial value";
+
+    // Output a variable
+    System.out.println(message);
+
+    // modify the contents of the variable
+    message = "Modified value";
+
+    // Output the updated variable
+    System.out.println(message);
+
+  } // end main
+} // end class
