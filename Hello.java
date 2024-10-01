9748484a88a9d45c92b6a6c0616888ad9c5115e4
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-10-01 17:02:19 +0100

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+//Class declaration
+
+class Hello {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+    
+  }  //end of main method
+ 
+}//end of class
