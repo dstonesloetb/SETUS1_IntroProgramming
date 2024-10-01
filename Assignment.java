f7674d759cd8638978b8bf5910e57af0a0949a14
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-10-01 17:14:18 +0100

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,18 @@
+class Assignment {
+  public static void main(String[] args) {
+
+    //Initialise two String variables
+    String txt = "Super ";
+    String lang = "Java";
+
+    //Add and assign 
+    txt += lang; //txt=txt+lang;
+    System.out.println(txt);
+
+    //Initialise 2 integers 
+    int sum=10;
+    int num = 20;
+    sum += num;   //sum=sum+num 
+    System.out.println(sum);
+  } //end main 
+} //end class
