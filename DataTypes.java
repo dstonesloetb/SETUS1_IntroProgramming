3ca16fbbefc677a8df30013254c39425c26ea3f1
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2024-10-01 17:09:44 +0100

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,19 @@
+class DataTypes {
+  public static void main(String[] args) {
+
+    //Declare and initialise 5 variables
+    char letter = 'M';
+    String title = "Java in easy steps";
+    int number = 365;
+    float decimal = 98.6f;
+    boolean result = true;
+
+    //Output the value of each variable 
+    System.out.println("Initial is: "+letter);
+    System.out.println("Book is: "+title);
+    System.out.println("Days are: "+number);
+    System.out.println("Temp is: "+decimal);
+    System.out.println("Answer is: "+result);
+
+  } //end main 
+} //end class 
